3962c5f12d7e557f7d15ba4cca00d9eedbe92cf1
Corwin40 <xavier.burke@openpixl.fr>
2026-02-03 08:06:00 +0100

    DEV | Lien cliquable carousel

--- src/Controller/App/DashboardController.php
+++ src/Controller/App/DashboardController.php
@@ -46,6 +46,10 @@ class DashboardController extends AbstractController
     #[Route(path: '/app/{slug}', name: 'op_webapp_page')]
     public function showPage(EntityManagerInterface $entityManager, $slug): \Symfony\Component\HttpFoundation\Response
     {
+        $config = $entityManager->getRepository(Config::class)->find(1);
+        if($config->getIsOffline() == 1){
+            return $this->render('app/offline.html.twig');
+        }
         $page = $entityManager->getRepository(Page::class)->findOneBy(['slug' => $slug]);
 
         if (!$page) {
@@ -81,8 +85,12 @@ class DashboardController extends AbstractController
      * Affiche automatiquement la page d'acceuil
      */
     #[Route(path: '/offline', name: 'op_webapp_public_offline')]
-    public function offline(): Response
+    public function offline(EntityManagerInterface $entityManager): Response
     {
+        $config = $entityManager->getRepository(Config::class)->find(1);
+        if($config->getIsOffline() == 0){
+            return $this->redirectToRoute('op_webapp_home');
+        }
         return $this->render('app/offline.html.twig');
     }
 
